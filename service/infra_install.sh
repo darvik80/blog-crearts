e3370af058c7e2f0be68554e46f0c0cdbecbac53
Ivan Kishchenko <darvik@crearts.xyz>
2020-03-29 19:33:27 +0700

    fix enable infra service

--- service/infra_install.sh
+++ service/infra_install.sh
@@ -9,7 +9,7 @@ service infra stop
 echo "# Setup service"
 cp -f service/infra.service /etc/systemd/system/infra.service
 systemctl daemon-reload
-systemctl enable core
+systemctl enable infra
 
 echo "# Start service"
 service infra start
