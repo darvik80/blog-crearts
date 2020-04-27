948af0f9eaf0317962a159e1b3fdd0b76c9a3cad
ivan.kishchenko <ivan.kishchenko@alibaba-inc.com>
2020-04-27 11:58:49 +0700

    tables names

--- core-service/src/main/java/xyz/crearts/blog/core/r2dbc/entry/FileDO.java
+++ core-service/src/main/java/xyz/crearts/blog/core/r2dbc/entry/FileDO.java
@@ -4,12 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
 
 import java.sql.Date;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Table("file")
 public class FileDO {
     @Id
     private Long id;
--- core-service/src/main/java/xyz/crearts/blog/core/r2dbc/entry/MimeTypeDO.java
+++ core-service/src/main/java/xyz/crearts/blog/core/r2dbc/entry/MimeTypeDO.java
@@ -4,10 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Table("mime_type")
 public class MimeTypeDO {
     @Id
     private Long id;
