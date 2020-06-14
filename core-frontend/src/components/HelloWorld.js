84febc7cce45ce010002fec10cc4948fa77d7851
Ivan Kishchenko <darvik@crearts.xyz>
2020-06-14 21:24:52 +0700

    upgrade websocket-extension

--- core-frontend/src/components/HelloWorld.js
+++ core-frontend/src/components/HelloWorld.js
@@ -1,10 +1,6 @@
 import React from "react";
 
 export default class HelloWorld extends React.Component {
-    constructor(props, context) {
-        super(props, context);
-    }
-
     render() {
         return <p>Hello, {this.props.mainTarget}</p>;
     }
