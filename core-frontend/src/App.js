8aceabe18734128d789dfea0daf33c87f9d7ceb8
Ivan Kishchenko <darvik@crearts.xyz>
2020-06-14 21:02:06 +0700

    upgrade react-js

--- core-frontend/src/App.js
+++ core-frontend/src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import HelloWorld from './components/HelloWorld';
+import Letter from './components/Letter';
+
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Container from 'react-bootstrap/Container';
+import {Button,Alert} from "react-bootstrap";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Container className="p-3">
+            <Jumbotron>
+                <h1 className="header">Welcome To React-Bootstrap</h1>
+                <img src={logo} className="App-logo" alt="logo"/>
+                <HelloWorld mainTarget="Ivan"/>
+                <div>
+                    <Letter bgcolor="#58b3ff">H</Letter>
+                    <Letter bgcolor="#ff605f">e</Letter>
+                    <Letter bgcolor="#ffd52e">l</Letter>
+                    <Letter bgcolor="#49dd8e">l</Letter>
+                    <Letter bgcolor="#ae99ff">o</Letter>
+                </div>
+                <div><Button size="lg" variant="info">Submit</Button></div>
+
+                <Alert variant="info">
+                    Just a message
+                </Alert>
+            </Jumbotron>
+        </Container>
+    );
 }
 
 export default App;
